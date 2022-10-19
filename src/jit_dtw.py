a4dd18928857f02de5d01245ea8a91296385bac4
Santiago <sanagurcia@gmail.com>
2022-10-19 23:09:44 +0200

    Jitting for dtw done!

--- src/jit_dtw.py
+++ src/jit_dtw.py
@@ -1,8 +1,11 @@
 """
 JIT DTW Module
-TODO:
-    jitted method for only returning cost
-    jitted method for extracting path
+
+This module offers a speed up 1-2 orders of magnitude over non jitted DTW module!
+
+Functions:
+    * dtw - compute DTW on two sequences, returning cost & warping path (30x speedup)
+    * dtw_cost - only compute and return cost (300x speedup)
 """
 
 import numpy as np
@@ -10,7 +13,7 @@ from numba import njit
 
 
 def dtw(a: np.ndarray, b: np.ndarray) -> (float, np.ndarray):
-    """Compute JIT DTW
+    """Compute JIT DTW: return cost & warping path
 
     Args:
         a (ndarray): First sequence
@@ -20,11 +23,12 @@ def dtw(a: np.ndarray, b: np.ndarray) -> (float, np.ndarray):
         (float, 1D array): min cost, warping path
     """
 
-    D, _ = compute_dtw_array(a, b)  # compute DTW arrays
-    return D[a.size - 1, b.size - 1]  # return total cost
+    D, P = compute_dtw_array(a, b)  # compute DTW arrays
+    wp = get_warping_path(P)
+    return D[a.size - 1, b.size - 1], wp  # return total cost, warping path
 
 
-# 20-30x speed up!
+# 20-30x speed up over non-jitted method!
 @njit
 def compute_dtw_array(a: np.ndarray, b: np.ndarray) -> (np.ndarray, np.ndarray):
     """Populate D(istance) and P(ath) arrays for two sequences, using euclidean distance"""
@@ -79,33 +83,80 @@ def compute_dtw_array(a: np.ndarray, b: np.ndarray) -> (np.ndarray, np.ndarray):
     return D, P
 
 
-# def dtw_cost_path(D: np.ndarray) -> (float, np.ndarray):
-#     """Extract total cost and warping path from DTW array
+@njit  # 10-20x speedup over non-jitted method
+def get_warping_path(P: np.ndarray) -> (np.ndarray):
+    """Extract warping path from Path array
 
-#     Args:
-#         D (np.ndarray): Computed DTW array
+    Args:
+        P (np.ndarray): NxNx2 array from compute_dtw_array
 
-#     Returns:
-#         (float, 1D-array): total cost, list of warping path indices
-#     """
+    Returns:
+        np.ndarray: Nx2 array
+    """
+    # P is a seq_a x seq_b array, with 2-length-tuples as entries
 
-#     m = D.shape[0]
-#     n = D.shape[1]
+    # longest possible path is P length + width
+    wp = np.zeros((P.shape[0] + P.shape[1], 2), dtype="uint16")
 
-#     # get total cost
-#     dtw_distance = D[m - 1, n - 1][0]
+    # indices for end-cell in P
+    i = P.shape[0] - 1
+    j = P.shape[1] - 1
 
-#     # get warping path
-#     i = [m - 1, n - 1]  # end-index on path (i, j)
-#     warping_path = [i]  # initialize path with end-index
+    # init warping path with last indices
+    wp[0] = [i, j]
 
-#     # construct warping-path by traversing DTW array backwards
-#     while not (i[0] == 0 and i[1] == 0):
-#         j = i[0]
-#         k = i[1]
-#         i = D[j][k][1]  # move index to predecessor
-#         warping_path.insert(0, i)  # push new index into stack
+    k = 1
+    while not (i == 0 and j == 0):
+        wp[k] = P[i, j]  # insert predecessor into warping path
+        i = wp[k, 0]  # move indices i, j based on
+        j = wp[k, 1]
+        k += 1
 
-#     warping_path = np.asarray(warping_path)  # convert list to np.array
+    return wp[:k]
 
-#     return (dtw_distance, warping_path)
+
+@njit  # 10x speed up over jitted-dtw with warping path
+def dtw_cost(a: np.ndarray, b: np.ndarray) -> float:
+    """Get DTW distance between two sequences
+
+    Args:
+        a (np.ndarray): first sequence
+        b (np.ndarray): second sequence
+
+    Returns:
+        float: total distance
+    """
+
+    D = np.zeros((a.size, b.size), dtype="float32")
+
+    # init first cell
+    D[0, 0] = abs(a[0] - b[0])
+
+    # calculate dtw for first column
+    for i in range(1, a.size):
+        D[i, 0] = abs(a[i] - b[0]) + D[i - 1, 0]  # dtw-cost := current-cost + predecessor-cost
+
+    # calculate dtw for first row
+    for j in range(1, b.size):
+        D[0, j] = abs(a[0] - b[j]) + D[0, j - 1]  # dtw-cost := current-cost + predecessor-cost
+
+    # for all other entries
+    for i in range(1, a.size):
+        for j in range(1, b.size):
+            d0 = D[i - 1, j]  # top
+            d1 = D[i, j - 1]  # left
+            d2 = D[i - 1, j - 1]  # diagonal
+
+            # Find min predecessor
+            d_min = d0
+            if d1 < d0:
+                if d1 < d2:
+                    d_min = d1
+                else:
+                    d_min = d2
+            elif d2 < d0:
+                d_min = d2
+
+            D[i, j] = abs(a[i] - b[j]) + d_min  # store cost
+
+    return D[a.size - 1, b.size - 1]
