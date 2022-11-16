8f7c36d39bda735b45d221955ba3501926cc5b66
Santiago <sanagurcia@gmail.com>
2022-11-16 11:10:05 +0100

    path

--- speed/main.py
+++ speed/main.py
@@ -1,10 +1,10 @@
 from src import utils, fast_dtw
 from src.dataset import Dataset
-from tslearn.metrics import dtw
+from tslearn.metrics import dtw, dtw_path
 
 # define globals: functions to be compared
-F1 = fast_dtw.dtw_cost
-F2 = dtw
+F1 = fast_dtw.dtw_path
+F2 = dtw_path
 
 
 def compare_speeds():
@@ -13,11 +13,14 @@ def compare_speeds():
     a = ds.train_set[0]
     b = ds.train_set[1]
 
-    # calculate DTW cost for sanity check
-    c1 = F1(a, b)
-    c2 = F2(a, b)
-    print(f"Cost from F1: {round(c1, 2)}, cost from F2: {round(c2, 2)}")
+    p1 = F1(a, b)
+    p2 = F2(a, b)
+    print("path 1:")
+    print(p1[:10])
+    print("path 2:")
+    print(p2[:10])
 
+    # calculate DTW cost for sanity check
     wrapped_F1 = utils.time_it(F1)
     wrapped_F2 = utils.time_it(F2)
 
