6b33e3e879abf41df795fd6de1aba8a98e947752
Santiago <sanagurcia@gmail.com>
2022-11-24 08:41:35 +0100

    ddtw shape descriptor

--- src/shapedtw.py
+++ src/shapedtw.py
@@ -1,34 +1,55 @@
 import math
 import numpy as np
 from src.fast_dtw import multi_dtw_cost, multi_dtw_path
+from src.fast_dtw import dtw_cost, dtw_path
 
 
-DESCRIPTOR = "IDENTITY"
-L = 7
+DESCRIPTOR = "DERIVATIVE"
+L = 5
 
 
-def shapedtw_path(seq_a: np.ndarray, seq_b: np.ndarray):
+def shapedtw_path(seq_a: np.ndarray, seq_b: np.ndarray) -> np.ndarray:
     """Return shape dtw alignment between two sequences"""
     sd_a = to_shape_descriptors(seq_a)
     sd_b = to_shape_descriptors(seq_b)
 
+    if len(sd_a.shape) == 1:  # if shape descriptors array 1D
+        return dtw_path(sd_a, sd_b)
+
     return multi_dtw_path(sd_a, sd_b)
 
 
-def shapedtw_cost(seq_a: np.ndarray, seq_b: np.ndarray):
+def shapedtw_cost(seq_a: np.ndarray, seq_b: np.ndarray) -> float:
     """Return shape dtw distance between two sequences"""
     sd_a = to_shape_descriptors(seq_a)
     sd_b = to_shape_descriptors(seq_b)
 
+    if len(sd_a.shape) == 1:  # if shape descriptors array 1D
+        return dtw_cost(sd_a, sd_b)
+
     return multi_dtw_cost(sd_a, sd_b)
 
 
-def to_shape_descriptors(seq: np.ndarray) -> (np.ndarray):
+def to_shape_descriptors(seq: np.ndarray) -> np.ndarray:
     """Return 2D shape descriptor mapping from given 1D sequence"""
 
-    subsequences = sample_subsequences(seq, L)
+    if DESCRIPTOR == "RAW_SUBSEQUENCE":
+        return sample_subsequences(seq, L)
 
-    return subsequences
+    if DESCRIPTOR == "DERIVATIVE":
+        return derivative_descriptor(seq, L)
+
+
+def derivative_descriptor(seq: np.ndarray, l: int) -> np.ndarray:
+    """Return shape descriptors corresponding to DDTW"""
+
+    subsequences = sample_subsequences(seq, l)
+    descriptors = np.zeros(subsequences.shape[0], dtype="float32")  # 1D array
+    for i in range(subsequences.shape[0]):
+        # take difference between each two points in subsequence, average the differences
+        descriptors[i] = np.mean(np.diff(subsequences[i]))
+
+    return descriptors
 
 
 def sample_subsequences(seq: np.ndarray, l: int) -> np.ndarray:
@@ -62,10 +83,9 @@ def pad_ends(seq: np.ndarray, l: int) -> np.ndarray:
 
 
 def main():
-    a = np.arange(10)
-    s = sample_subsequences(a, 6)
-    print(a)
-    print(s)
+    a = np.arange(2, 12)
+    sd = to_shape_descriptors(a)
+    print(sd)
 
 
 if __name__ == "__main__":
